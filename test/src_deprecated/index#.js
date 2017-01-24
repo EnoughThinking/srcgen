9b385466a2f1bcbed09d80675bcc73ce0e1dabd0
EnoughThinking <justinsetonpro1994@gmail.com>
2017-01-24 22:22:57 +0500

    update extractor, add prompts for files in folder

--- test/src_deprecated/index#.js
+++ test/src_deprecated/index#.js
@@ -1,21 +1,23 @@
 var generator = require('../../index');
 var path = require('path');
-var result=
-generator.extractor.scan(path.resolve(__dirname, './index#.html'));
-
-console.log(
-    /*
-    generator.builder.template(
-        path.resolve(__dirname, './frontend/index#.html'),
-        { project: { pages: [{ name: 1, title: 2 }, { name: 3, title: 4 }] } }
-    )*/
-    1//result
-/*generator.extractor.scan(path.resolve(__dirname, './index#.html'))
-/*generator.builder.folder.all(
-        path.resolve(__dirname),
-        path.resolve(__dirname, '../dest'),
-        { project: "3" },
-        [__filename]
-    )*/
-);
-//generator.extractor.folder.scan(path.resolve(__dirname));
+var result =
+    /*generator.extractor.scan(path.resolve(__dirname, './index#.html'));
+    */
+    console.log(
+        /*
+        generator.builder.template(
+            path.resolve(__dirname, './frontend/index#.html'),
+            { project: { pages: [{ name: 1, title: 2 }, { name: 3, title: 4 }] } }
+        )*/
+        //1//result
+        /*generator.extractor.scan(path.resolve(__dirname, './index#.html'))
+        /*generator.builder.folder.all(
+                path.resolve(__dirname),
+                path.resolve(__dirname, '../dest'),
+                { project: "3" },
+                [__filename]
+            )*/
+    );
+generator.extractor.folder.prompt(path.resolve(__dirname, '../fixtures/src')).then(function (data) {
+    console.log(data.data, 1, data.variables.STRVALUE[1]);
+});
