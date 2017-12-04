2507fd9e1a7a6813db55d75a41563ab8ba8587eb
EnoughThinking <justinsetonpro1994@gmail.com>
2017-12-04 15:38:16 +0500

    Merge conflict

--- bin/srcgen.js
+++ bin/srcgen.js
@@ -7,7 +7,7 @@ var figlet = require('figlet');
 var program = require('commander');
 
 program
-    .version('0.2.12')
+    .version('0.3.1')
     .option('-l, --show-templates', 'Show exists templates')
     .option('-t, --build-template [value]', 'Build from template')
     .option('-f, --input-file [value]', 'File name with input data')
